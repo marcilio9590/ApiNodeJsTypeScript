529f8854ede193397fb6a424df24f2d874c66693
Marcilio Cobel <marcilio.c.junior@accenture.com>
2019-03-17 16:16:30 -0300

    modelando retorno

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Put, Delete, Param, Body } from "@nestjs/common";
 import { Customer } from "../models/customer.model";
+import { Result } from "../models/result.model";
 
 // localhost:3000/v1/customer
 @Controller('v1/customers')
@@ -7,29 +8,26 @@ export class CustomerController {
 
     @Get()
     get() {
-        return 'Obter os cliente';
+        return new Result(null, true, [], null);
     }
 
     @Get(':document')
     getById(@Param('document') document) {
-        return 'Obter o cliente ' + document;
+        return new Result(null, true, {}, null);
     }
 
     @Post()
     post(@Body() body: Customer) {
-        return body.name;
+        return new Result('Cliente Criado com Sucesso', true, body, null);
     }
 
     @Put(':document')
     put(@Body() body: Customer, @Param('document') document) {
-        return {
-            customer: document,
-            data: body
-        };
+        return new Result('Cliente Alterado com Sucesso', true, body, null);
     }
 
     @Delete(':document')
     delete(@Param('document') document) {
-        return 'Remover o cliente ' + document;
+        return new Result('Cliente Removido com Sucesso', true, null, null);
     }
 }
