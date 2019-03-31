d2bd9b5351c80fcca897f8c4eb355a132b3365f5
Marcilio Cobel <marcilio.c.junior@accenture.com>
2019-03-30 23:53:29 -0300

    inject services

--- src/modules/store/store.module.ts
+++ src/modules/store/store.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ProductController } from './controllers/product.controller';
-import { ProductService } from './services/product.service';
-import { Product } from './entities/product.entity';
+import { ProductController } from 'src/modules/store/controllers/product.controller';
+import { ProductService } from 'src/modules/store/services/product.service';
+import { Product } from 'src/modules/store/entities/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from 'src/modules/store/entities/order.entity';
+import { OrderItem } from 'src/modules/store/entities/order-item.entity';
+import { OrderService } from 'src/modules/store/services/order.service';
+import { OrderItemService } from 'src/modules/store/services/order-item.service';
+import { OrderController } from 'src/modules/store/controllers/order.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([
-        Product
+        Product,
+        Order,
+        OrderItem
     ])],
     providers: [
-        ProductService
+        ProductService,
+        OrderService,
+        OrderItemService
     ],
     controllers: [
-        ProductController
+        ProductController,
+        OrderController
     ],
 })
 export class StoreModule { }
